68f5d66bba713faa458f11abc4d7c8addbdacfb7
Shreyas0134 <shreyaskarandikar2000@gmail.com>
2022-12-08 14:51:42 +0530

    Updated

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p>&copy; 2022 Cabinate Admin Panel.</p>
+    </div>
+        <!--//footer-->
---
+++ hello.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i%7cMontserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<body>
+
+  <?php include_once('includes/header.php');?>
+<center><p><h6>hey this  is  a demo page</p></center></h6>	
+ <?php include_once('includes/footer.php');?>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menumaker.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/sticky-header.js"></script>
+</body>
+
+</html>
